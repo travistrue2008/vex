3cfa6f7ab2cadeb4e5a5f54f510dc748e180fa56
travistrue2008 <travis.true08@gmail.com>
2020-04-22 00:26:17 -0700

    Exposed all public entities at the top-level

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub mod common;
-pub mod matrix2;
-pub mod matrix3;
-pub mod matrix4;
-pub mod vector2;
-pub mod vector3;
-pub mod vector4;
+mod common;
+mod matrix2;
+mod matrix3;
+mod matrix4;
+mod vector2;
+mod vector3;
+mod vector4;
 
-pub use matrix2::Matrix2::*;
-pub use matrix3::Matrix3::*;
-pub use matrix4::Matrix4::*;
-pub use vector2::Vector2::*;
-pub use vector3::Vector3::*;
-pub use vector4::Vector4::*;
+pub use matrix2::*;
+pub use matrix3::*;
+pub use matrix4::*;
+pub use vector2::*;
+pub use vector3::*;
+pub use vector4::*;
